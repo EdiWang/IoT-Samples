c4266ab7b2f5aef156bd90891defddbf7944bf33
Edi Wang <Edi.Wang@outlook.com>
2016-05-05 20:52:48 +0800

    Add Reed Light Project

---
+++ WindowsIoTReedLight/WindowsIoTReedLight/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Windows.Devices.Gpio;
+using Windows.UI.Xaml.Controls;
+
+namespace WindowsIoTReedLight
+{
+    public sealed partial class MainPage : Page
+    {
+        public GpioController GpioController { get; set; }
+
+        public GpioPin ReedPin { get; set; }
+
+        public GpioPin LedPin { get; set; }
+
+        public bool IsLightOn { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            GpioController = GpioController.GetDefault();
+            if (null != GpioController)
+            {
+                ReedPin = GpioController.OpenPin(5);
+                ReedPin.SetDriveMode(GpioPinDriveMode.Input);
+                ReedPin.ValueChanged += (sender, args) =>
+                {
+                    if (ReedPin.Read() == GpioPinValue.Low)
+                    {
+                        IsLightOn = !IsLightOn;
+
+                        LedPin.Write(IsLightOn ? GpioPinValue.Low : GpioPinValue.High);
+                    }
+                };
+
+                LedPin = GpioController.OpenPin(6);
+                LedPin.SetDriveMode(GpioPinDriveMode.Output);
+                LedPin.Write(GpioPinValue.High);
+            }
+        }
+    }
+}
